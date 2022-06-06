cef73c43c70cbd9d69e755ec8e7ef414abf5ce55
vit-ganich <vitali.hanich@gmail.com>
2022-06-06 14:58:43 +0200

    Added headless run

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -86,6 +86,9 @@ export const config: Options.Testrunner = {
       //
       browserName: 'chrome',
       acceptInsecureCerts: true,
+      'goog:chromeOptions': {
+        args: ['headless', 'disable-gpu'],
+      },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
